b492b823e61bf5108f4fb38c6945a7f9c2454bfd
MCris29 <crismax0629@gmail.com>
2021-06-29 22:28:56 -0500

    CRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,34 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { RouteReuseStrategy } from '@angular/router';
+
+// import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+// import { AppComponent } from './app.component';
+// import { AppRoutingModule } from './app-routing.module';
+
+// //Firebase
+// import { AngularFireModule } from '@angular/fire';
+// import {AngularFireDatabaseModule} from '@angular/fire/database'
+
+// //Enviroment
+// import { environment } from 'src/environments/environment';
+
+// @NgModule({
+//   declarations: [AppComponent],
+//   entryComponents: [],
+//   imports: [
+//     BrowserModule, 
+//     IonicModule.forRoot(), 
+//     AppRoutingModule,
+//     AngularFireModule.initializeApp(environment),
+//     AngularFireDatabaseModule
+//   ],
+//   providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule {}
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -7,11 +38,31 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+//  Firebase modules
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+
+// Environment
+import { environment } from '../environments/environment';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule
+  ],
+  providers: [
+    { 
+      provide: RouteReuseStrategy, 
+      useClass: IonicRouteStrategy 
+    }
+    ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
